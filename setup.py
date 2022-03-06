3dd3b13c3615bd54eae856929196e37812379c33
Or Koren <or.koren@shutterfly.com>
2022-03-06 10:49:01 +0200

    fix version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 from comparing_distributions import __version__
 
 setup(
-    name='comparing_distributions',
+    name='DistComp',
     version='0.1.0',
     description=("a tool for comparing disterbutions"
-    url='https://gh.internal.shutterfly.com/or-koren/comparing_distributions',
+    url='https://gh.internal.shutterfly.com/or-koren/DistComp',
     author='OR KOREN',
     author_email='or.koren@shutterfly.com',
     py_modules=['DistComp'],

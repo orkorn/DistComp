89a1165e9b3b1b0929692835212862faddf77d46
Or Koren <or.koren@shutterfly.com>
2022-03-06 10:09:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ from comparing_distributions import __version__
 
 setup(
     name='comparing_distributions',
-    version=__version__,
+    version=0.1.0,
+    description=("a tool for comparing disterbutions"
     url='https://gh.internal.shutterfly.com/or-koren/comparing_distributions',
     author='OR KOREN',
     author_email='or.koren@shutterfly.com',
     py_modules=['comparing_distributions'],
-)
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'plotly',
+        'scipy',
+        'seaborn',
+        'matplotlib',
+        'termcolor']
 
+    )
 
-# install_requires=[
-#     'pandas','numpy','plotly','scipy','seaborn','matplotlib','termcolor',''
-# ],
-
- 
-  

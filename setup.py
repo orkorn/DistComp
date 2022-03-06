0c15e4f6432930b1182c571315096220323376f9
Or Koren <or.koren@shutterfly.com>
2022-03-06 10:47:27 +0200

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from comparing_distributions import __version__
 
 setup(
     name='comparing_distributions',
-    version=0.1.0,
+    version='0.1.0',
     description=("a tool for comparing disterbutions"
     url='https://gh.internal.shutterfly.com/or-koren/comparing_distributions',
     author='OR KOREN',
